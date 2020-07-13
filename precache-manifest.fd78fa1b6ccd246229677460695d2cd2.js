79d4ecc52720ba77d3d082af9548648f40149697
Irina Lanskaia <irina.lanskaia@nixsolutions.com>
2020-07-13 19:15:13 +0300

    Updates

---
+++ precache-manifest.fd78fa1b6ccd246229677460695d2cd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d1f3875985c22c94e08dc6c155c5c9",
+    "url": "/MoviesApp/index.html"
+  },
+  {
+    "revision": "4abd26a0e9f3977a99d1",
+    "url": "/MoviesApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "da63a85096dbd408885f",
+    "url": "/MoviesApp/static/css/main.b6266b9a.chunk.css"
+  },
+  {
+    "revision": "4abd26a0e9f3977a99d1",
+    "url": "/MoviesApp/static/js/2.92bededb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MoviesApp/static/js/2.92bededb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da63a85096dbd408885f",
+    "url": "/MoviesApp/static/js/main.415f1e29.chunk.js"
+  },
+  {
+    "revision": "cad784bc278312c40542",
+    "url": "/MoviesApp/static/js/runtime-main.ee8f6a40.js"
+  }
+]);
